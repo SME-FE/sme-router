d99fdf71b8e406208a7b5d731fac1b994a42dc8b
hwen <hwenleung@gmail.com>
2017-10-22 23:26:00 +0800

    ignore test ...

--- karma.conf.js
+++ karma.conf.js
@@ -1,17 +1,15 @@
 // Karma configuration
 // Generated on Thu Oct 19 2017 20:10:47 GMT+0800 (中国标准时间)
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha'],
-
 
     // list of files / patterns to load in the browser
     files: [
@@ -19,24 +17,22 @@ module.exports = function(config) {
       'src/**/*.js'
     ],
 
-
     // list of files to exclude
     exclude: [
     ],
 
-
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'test/*.test.js': ['webpack'],
-      'src/**/*.js': ['webpack']
+      'src/**/*.js': ['webpack'],
+      'test/*.test.js': ['webpack']
     },
 
     plugins: [
       'karma-webpack',
       'karma-mocha',
       'karma-chrome-launcher',
-      'karma-coverage',
+      'karma-coverage'
     ],
     webpack: require('./build/webpack.test.config'),
     webpackMiddleware: {
@@ -49,7 +45,7 @@ module.exports = function(config) {
         // reporters not supporting the `file` property
         { type: 'html', subdir: 'report-html' },
         { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
-        { type: 'text-summary', subdir: '.', file: 'text-summary.txt' },
+        { type: 'text-summary', subdir: '.', file: 'text-summary.txt' }
       ]
     },
 
@@ -58,23 +54,18 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress', 'coverage'],
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
